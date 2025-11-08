aff556e975b9c52aa26299ab551af8ebc29d830e
karabas21 <karabas21@itu.edu.tr>
2025-11-08 19:49:35 +0300

    added db.py + .env.example, updated requirements and cleaned up files a bit by gulbahar

--- App/db.py
+++ App/db.py
@@ -1,2 +1,24 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
 db = SQLAlchemy()
+
+def init_db(app):
+    #it will read from .env
+    db_user = os.getenv("DB_USER")
+    db_pass = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST", "localhost")
+    db_port = os.getenv("DB_PORT", "3306")
+    db_name = os.getenv("DB_NAME")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+def ping_db():
+    # a basic connection test
+    with db.engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
