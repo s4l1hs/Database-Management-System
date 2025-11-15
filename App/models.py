3d5387d036aa48ebb225dda8bdf724f6f2da9042
salihsefer36 <sefersalih017@gmail.com>
2025-11-15 16:09:35 +0300

    SustainabilityData & SustainabilityIndicatorDetails Added to models.py

--- App/models.py
+++ App/models.py
@@ -24,4 +24,28 @@ class EnergyData(db.Model):
     __table_args__ = (
         db.UniqueConstraint('country_id', 'indicator_id', 'year', name='unique_energy_data'),
     )
-  
+
+class SustainabilityIndicatorDetails(db.Model):
+    __tablename__ = 'sustainability_indicator_details'
+    
+    indicator_id = db.Column(db.Integer, primary_key=True)
+    indicator_name = db.Column(db.String(255), nullable=False, unique=True)
+    indicator_code = db.Column(db.String(50), nullable=False, unique=True)
+    source_note = db.Column(db.Text)
+    unit_of_measure = db.Column(db.String(50))
+    
+    data_points = db.relationship("SustainabilityData", backref="indicator_details")
+
+class SustainabilityData(db.Model):
+    __tablename__ = 'sustainability_data'
+    
+    data_id = db.Column(db.Integer, primary_key=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.country_id'), nullable=False)
+    indicator_id = db.Column(db.Integer, db.ForeignKey('sustainability_indicator_details.indicator_id'), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    value = db.Column(db.Float)
+    
+    __table_args__ = (
+        db.UniqueConstraint('country_id', 'indicator_id', 'year', name='unique_sustainability_data'),
+    )
+
