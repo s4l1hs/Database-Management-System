e5cdef91df48a476434d051894a3d353e32e4f76
mtuncer4 <163677608+mtuncer4@users.noreply.github.com>
2025-11-16 21:43:07 +0300

    Add freshwater route to list freshwater data

---
+++ App/routes/freshwater.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template
+from db import get_db
+
+
+freshwater_bp = Blueprint("freshwater", __name__)
+@freshwater_bp.route("/", methods=["GET"])
+def list_freshwater():
+    db = get_db()
+    cur = db.cursor(dictionary=True)
+
+    cur.execute("""
+        SELECT f.data_id,
+               c.country_name,
+               c.region,
+               c.country_code,
+               i.indicator_name,
+               i.indicator_description,
+               i.unit_of_measure,
+               f.year,
+               f.value
+        FROM freshwater_data f
+        JOIN countries c ON c.country_id = f.country_id
+        JOIN freshwater_indicator_details i ON i.indicator_id = f.indicator_id
+        ORDER BY c.country_name, i.indicator_name, f.year
+    """)
+
+    rows = cur.fetchall()
+    
+   
+    return render_template("freshwater_list.html", rows=rows)
+
+  
