0bb0005613e9e6a2d715bcd1e9645b64e739fee0
salihsefer36 <sefersalih017@gmail.com>
2025-11-23 21:20:02 +0300

    Data Initialization Scripts Revised
    __init__.py and main.py revised to start the database without any problem

--- App/routes/__init__.py
+++ App/routes/__init__.py
@@ -1,1 +1,26 @@
-#necessary for importing
+from flask import Flask
+from App.db import db, close_db
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:db_pass@localhost/wdi_project'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Session and Flash messages secret key for security (required)
+    app.secret_key = "secret_key_wdi_team1"
+
+    # Start the ORM
+    db.init_app(app)
+
+    app.teardown_appcontext(close_db)
+
+    from App.routes.sustainability import sustainability_bp
+    app.register_blueprint(sustainability_bp)
+
+    from flask import redirect, url_for
+    @app.route('/')
+    def index():
+        return redirect(url_for('sustainability.list_sustainability'))
+
+    return app
