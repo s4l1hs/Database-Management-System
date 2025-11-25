ef1676d06194730ff83fae45af9e864b0c6f9cba
salihsefer36 <sefersalih017@gmail.com>
2025-11-25 14:15:54 +0300

    CSV & SQL scripts for Sustainability

---
+++ SQL/load_sustainability_indicator_details.sql
@@ -0,0 +1,9 @@
+USE sustainability;
+-- Dosya yolunu kendi bilgisayarına göre güncellemeyi unutma!
+LOAD DATA LOCAL INFILE '/Users/fahrettin/Desktop/Data-insertion/sustainability_indicator_details.csv'
+INTO TABLE sustainability_indicator_details
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(sus_indicator_id, indicator_name, indicator_code, indicator_description);
